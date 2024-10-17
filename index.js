2c0697a35d6041a848f77d288048a27584728b89
Dauarim <greencode.kz@gmail.com>
2024-10-17 16:25:39 +0500

    finished products page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 8009;
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
