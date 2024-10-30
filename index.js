51008eb35b8e74f0c94de08fead139e8a0f55175
forlessonkz <forlessons.kz@gmail.com>
2024-10-31 02:14:40 +0600

    connetc local mongoDB and create db newBase

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
 
 
 const app = express();
@@ -8,14 +9,47 @@ const PORT = 8009;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect('mongodb://localhost:27017/newBase').then(()=> {
+    console.log('Connected by MongoDB')
+}).catch((e) => {
+    console.log('Failed to connect to MongoDB')
+});
+
+const newBaseSchema = new mongoose.Schema({
+    title: String,
+    discount: Number,
+    price: Number,
+    images: String,
+});
+
+const newBase = mongoose.model('newBase', newBaseSchema);
+
+app.post('/new', async(req, res) => {
+    if(req.body.title.length != 0){
+        await new newBase({
+            title: req.body.title,
+            discount: req.body.discount,
+            price: req.body.price,
+            images: req.body.images,
+        }).save()
+        res.redirect('/products')
+    } else {
+        res.redirect('/new?error=1')
+    }
+})
 
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/products', (req, res) => {
-    res.render('products')
+app.get('/products', async (req, res) => {
+    const data = await newBase.find()
+    console.log(data)
+    res.render('products', {data})
 })
 app.get('/new', (req, res) => {
     res.render('addProduct')
---
+++ public/js/actions.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productMenus = document.querySelectorAll(".product-menu");
+    const modalWindows = document.querySelectorAll(".menu-modal-window");
+
+    productMenus.forEach((menu, index) => {
+        menu.addEventListener("click", () => {
+            const modalWindow = modalWindows[index];
+            if (modalWindow.style.display === "none" || !modalWindow.style.display) {
+                modalWindow.style.display = "block";
+            } else {
+                modalWindow.style.display = "none";
+            }
+        });
+    });
+
+    // Закрытие модального окна при клике вне его
+    document.addEventListener("click", function (event) {
+        modalWindows.forEach(modal => {
+            if (!modal.contains(event.target) && !event.target.closest(".product-menu")) {
+                modal.style.display = "none";
+            }
+        });
+    });
+});
